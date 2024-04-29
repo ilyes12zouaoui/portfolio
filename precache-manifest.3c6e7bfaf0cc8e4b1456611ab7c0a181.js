fe0cb2d567b1e5099c5eac4a53ab674b3984eca8
ilyes12zouaoui <ilyes12zouaoui@users.noreply.github.com>
2024-04-29 13:39:26 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.3c6e7bfaf0cc8e4b1456611ab7c0a181.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb4a8d0139407e9b85f5764a84c990d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "96d1b3de41e3171d6dfa",
+    "url": "/portfolio/static/css/2.549f647d.chunk.css"
+  },
+  {
+    "revision": "b5713263a5e4cb6d647d",
+    "url": "/portfolio/static/css/main.c5388d9e.chunk.css"
+  },
+  {
+    "revision": "96d1b3de41e3171d6dfa",
+    "url": "/portfolio/static/js/2.ccd81cf6.chunk.js"
+  },
+  {
+    "revision": "b5713263a5e4cb6d647d",
+    "url": "/portfolio/static/js/main.bffd0717.chunk.js"
+  },
+  {
+    "revision": "d73719c56df2fda83fff",
+    "url": "/portfolio/static/js/runtime-main.fbe1e1db.js"
+  },
+  {
+    "revision": "36ddd6663cb4e52a8068499956a74e4f",
+    "url": "/portfolio/static/media/aws.36ddd666.png"
+  },
+  {
+    "revision": "76957cc884d3fc6b8312d03e67de9ddc",
+    "url": "/portfolio/static/media/ckad.76957cc8.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "40ea7ef95673f3215a7eed5237222dfe",
+    "url": "/portfolio/static/media/psd1.40ea7ef9.png"
+  },
+  {
+    "revision": "d1ba3fa831dd5662a598fe0814bab356",
+    "url": "/portfolio/static/media/psm1.d1ba3fa8.png"
+  },
+  {
+    "revision": "a1f90c233125862b5922457e1beddec2",
+    "url": "/portfolio/static/media/psm2.a1f90c23.png"
+  },
+  {
+    "revision": "891ec8f6b56e69c7764c1862beea0507",
+    "url": "/portfolio/static/media/pspo1.891ec8f6.png"
+  },
+  {
+    "revision": "d2f973e8890e022f6eca0e99bb6adc5f",
+    "url": "/portfolio/static/media/pspo2.d2f973e8.png"
+  },
+  {
+    "revision": "b9c49ba858cac7f35960f8778a1d205e",
+    "url": "/portfolio/static/media/spring.b9c49ba8.png"
+  },
+  {
+    "revision": "700bb598262e758d9e2a000a9a1f1695",
+    "url": "/portfolio/static/media/terraform002.700bb598.png"
+  }
+]);
